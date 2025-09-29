ff0d5ecf0425f36ca6d27c78d095f4c4d6c905ee
Hakim Firman Febrian <code.hakimfirman@gmail.com>
2025-09-29 14:59:21 +0700

    comment out database and auth middleware and user service

--- src/middleware/auth-middleware.js
+++ src/middleware/auth-middleware.js
@@ -1,25 +1,25 @@
-import {prismaClient} from "../application/database.js";
+// import {prismaClient} from "../application/database.js";
 
-export const authMiddleware = async (req, res, next) => {
-    const token = req.get('Authorization')
+// export const authMiddleware = async (req, res, next) => {
+//     const token = req.get('Authorization')
 
-    if(!token){
-        res.status(401).json({
-            errors:"Unauthorized"
-        }).end()
-    } else {
-        const user = await  prismaClient.user.findFirst({
-            where:{
-                token:token
-            }
-        })
-        if(!user){
-            res.status(401).json({
-                errors:"Unauthorized"
-            }).end()
-        }else {
-            req.user = user;
-            next();
-        }
-    }
-}
+//     if(!token){
+//         res.status(401).json({
+//             errors:"Unauthorized"
+//         }).end()
+//     } else {
+//         const user = await  prismaClient.user.findFirst({
+//             where:{
+//                 token:token
+//             }
+//         })
+//         if(!user){
+//             res.status(401).json({
+//                 errors:"Unauthorized"
+//             }).end()
+//         }else {
+//             req.user = user;
+//             next();
+//         }
+//     }
+// }
