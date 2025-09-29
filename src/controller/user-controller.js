9bbd560c7a81a4cf59e2e58db74e11687f775bdd
Hakim Firman Febrian <code.hakimfirman@gmail.com>
2025-09-29 15:04:15 +0700

    comment out user

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -1,72 +1,72 @@
-import userService  from "../service/user-service.js";
-import {logger} from "../application/logging.js";
-const register =async (req, res, next) => {
-    try {
-        const result =await userService.register(req.body)
-        res.status(200).json({
-            data:result
-        })
-    }catch (e){
-        next(e);
-    }
-}
+// import userService  from "../service/user-service.js";
+// import {logger} from "../application/logging.js";
+// const register =async (req, res, next) => {
+//     try {
+//         const result =await userService.register(req.body)
+//         res.status(200).json({
+//             data:result
+//         })
+//     }catch (e){
+//         next(e);
+//     }
+// }
 
 
-const login =  async (req,res,next) => {
-    try {
-        const result = await userService.login(req.body)
-        logger.info('----THIS IS RESULT FROM CONTROLLER ---')
-        logger.info(result)
-        res.status(200).json({
-            data:result
-        })
+// const login =  async (req,res,next) => {
+//     try {
+//         const result = await userService.login(req.body)
+//         logger.info('----THIS IS RESULT FROM CONTROLLER ---')
+//         logger.info(result)
+//         res.status(200).json({
+//             data:result
+//         })
 
-    }catch (e) {
-        next(e)
-    }
-}
+//     }catch (e) {
+//         next(e)
+//     }
+// }
 
-const get = async (req,res,next) => {
-    try{
-        const username = req.user.username;
-        const result = await userService.get(username)
-        res.status(200).json({
-            data:result
-        })
-    }catch (e) {
-        next(e)
-    }
-}
+// const get = async (req,res,next) => {
+//     try{
+//         const username = req.user.username;
+//         const result = await userService.get(username)
+//         res.status(200).json({
+//             data:result
+//         })
+//     }catch (e) {
+//         next(e)
+//     }
+// }
 
-const update = async (req, res, next) =>{
-    try{
-        const username = req.user.username;
-        const request = req.body;
-        request.username = username;
-        const result = await userService.update(request);
-        res.status(200).json({
-            data:result
-        });
-    }catch (e) {
-        next(e)
-    }
-}
+// const update = async (req, res, next) =>{
+//     try{
+//         const username = req.user.username;
+//         const request = req.body;
+//         request.username = username;
+//         const result = await userService.update(request);
+//         res.status(200).json({
+//             data:result
+//         });
+//     }catch (e) {
+//         next(e)
+//     }
+// }
 
-const logout = async (req, res, next) => {
-    try {
-        await userService.logout(req.user.username);
-        res.status(200).json({
-            data:"OK"
-        })
-    } catch (e) {
-        next(e)
-    }
-}
+// const logout = async (req, res, next) => {
+//     try {
+//         await userService.logout(req.user.username);
+//         res.status(200).json({
+//             data:"OK"
+//         })
+//     } catch (e) {
+//         next(e)
+//     }
+// }
 
-export default{
-    register,
-    login,
-    get,
-    update,
-    logout
-}
+// export default{
+//     register,
+//     login,
+//     get,
+//     update,
+//     logout
+// }
