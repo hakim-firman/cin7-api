50c3b7dae41cae3bde24a5a32b2bad4848ef4940
Hakim Firman Febrian <code.hakimfirman@gmail.com>
2025-09-29 14:15:07 +0700

    integrate with vercel

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-import {web} from "./application/web.js";
-import {logger} from "./application/logging.js";
+import { web } from "./application/web.js";
+import { logger } from "./application/logging.js";
 
-web.listen(3000, () => {
-    const port = process.env.PORT || 3000;
-    const appUrl = process.env.APP_URL || 'http://localhost';
-    console.log(`Server running at ${appUrl}:${port}/api`);
-});
+export default web;
