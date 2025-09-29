9d354092a46e010d54a539582253d3fe1831b8e3
Hakim Firman Febrian <code.hakimfirman@gmail.com>
2025-09-29 15:05:43 +0700

    remove user controller and routes

--- src/route/public-api.js
+++ src/route/public-api.js
@@ -1,10 +1,6 @@
 import express from "express";
-import userController from "../controller/user-controller.js";
 import { cin7Router } from "./cin7Router.js";
 const publicRouter = express.Router()
-
-publicRouter.post('/api/register',userController.register);
-publicRouter.post('/api/login',userController.login);
 
 publicRouter.use('/api/cin7', cin7Router);
 
